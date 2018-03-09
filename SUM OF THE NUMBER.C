7672502a4a0edf22d9ce4e34e9de247d32ed61e4
thulasideepak <35525691+thulasideepak@users.noreply.github.com>
2018-03-09 15:50:04 +0530

    Create SUM OF THE NUMBER.C
    SUM OF THE NUMBER

---
+++ SUM OF THE NUMBER.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,a=0;
+    printf("\n enter the number :");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        a=a+i;
+    }
+    printf("\n %d",a);
+    return 0;
+}
+
+
+
